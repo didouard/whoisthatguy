84f8c25f2d89b98460a44ca170fb576c9212318f
Edouard FERRARI <edouard@ferrari.wf>
2018-08-14 23:52:24 +0200

    feat: typeahead work with distant data

--- src/App.js
+++ src/App.js
@@ -1,54 +1,62 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Typeahead} from 'react-bootstrap-typeahead'; // ES2015
 
 class App extends Component {
-    state = {
-	multiple: false,
-    };
+    constructor(props) {
+	super(props);
+
+	this.state = {
+	    users: []
+	    , isLoading: true
+	};
+    }
+
+    componentDidMount() {
+	fetch('http://192.168.113.42:8080/users')
+	    .then(response => response.json())
+	    .then(data => this.setState({ users: data.users, isLoading: false }));
+    }
     
     render() {
-	const {multiple} = this.state;
-	const {options} = {};
-	
-	return (
-	    <div className="App">
-              <header className="App-header">
+	const {users, isLoading} = this.state;
+		
+	if (isLoading == true) return (<div>Loading... </div>);
+
+	return ( 
+		<div className="App">
+		<div>{this.test} hello</div>
+		<header className="App-header">
 		<img src={logo} className="App-logo" alt="logo" />
 		<h1 className="App-title">Welcome to React</h1>
-              </header>
-              <p className="App-intro">
-		To get started, edit <code>src/App.js</code> and save to reload.
-              </p>
-	      
-	      <div class="container">
+            </header>
+		<p className="App-intro">
+	    To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+	    
+		<div class="container">
 		<div class="row">
-		  <div class="col-sm">
-		    One of three columns
-		  </div>
-		  <div class="col-sm">
-		    One of three columns
-		  </div>
-		  <div class="col-sm">
-		    One of three columns
-		  </div>
-		</div>
-	      </div>
-	      <React.Fragment>
+		<div class="col-sm">
+	    One of three columns
+	    </div>
+		<div class="col-sm">
+	    One of three columns
+	    </div>
+		<div class="col-sm">
+	    One of three columns
+	    </div>
+	    </div>
+	    </div>
+		<Fragment>
 		<Typeahead
-		   labelKey="name"
-		   multiple={multiple}
-		   options={[
-		   {id: 1, name: 'John'},
-		   {id: 2, name: 'Miles'},
-		   {id: 3, name: 'Charles'},
-		   {id: 4, name: 'Herbie'}
-		   ]}
-		   placeholder="Choose a state..."
-		   />
-	
-		      </React.Fragment>
+	    labelKey="name"
+	    options={users}
+	    
+	    placeholder="Choose a state..."
+		/>
+	    
+	    </Fragment>
 	    </div>
 
 	);
