c51e1f646cc2d8ef279f955bf2e37c4e582cfa53
Edouard FERRARI <edouard@ferrari.wf>
2018-08-14 22:42:41 +0200

    feat: add typeahead

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Typeahead} from 'react-bootstrap-typeahead'; // ES2015
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    state = {
+	multiple: false,
+    };
+    
+    render() {
+	const {multiple} = this.state;
+	const {options} = {};
+	
+	return (
+	    <div className="App">
+              <header className="App-header">
+		<img src={logo} className="App-logo" alt="logo" />
+		<h1 className="App-title">Welcome to React</h1>
+              </header>
+              <p className="App-intro">
+		To get started, edit <code>src/App.js</code> and save to reload.
+              </p>
 	      
-	<div class="container">
-	  <div class="row">
-	    <div class="col-sm">
-	      One of three columns
+	      <div class="container">
+		<div class="row">
+		  <div class="col-sm">
+		    One of three columns
+		  </div>
+		  <div class="col-sm">
+		    One of three columns
+		  </div>
+		  <div class="col-sm">
+		    One of three columns
+		  </div>
+		</div>
+	      </div>
+	      <React.Fragment>
+		<Typeahead
+		   labelKey="name"
+		   multiple={multiple}
+		   options={[
+		   {id: 1, name: 'John'},
+		   {id: 2, name: 'Miles'},
+		   {id: 3, name: 'Charles'},
+		   {id: 4, name: 'Herbie'}
+		   ]}
+		   placeholder="Choose a state..."
+		   />
+	
+		      </React.Fragment>
 	    </div>
-	    <div class="col-sm">
-	      One of three columns
-	    </div>
-	    <div class="col-sm">
-	      One of three columns
-	    </div>
-	  </div>
-	</div>
-      </div>
 
-    );
-  }
+	);
+    }
 }
 
 export default App;
